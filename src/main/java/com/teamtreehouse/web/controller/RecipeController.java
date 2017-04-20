dce9f94bdc50dd84cfae0eb3b43972eb917043d8
David Riesz <dbriesz@users.noreply.github.com>
2017-04-20 15:27:01 -0700

    Create layout.html and application successfully loads index.html

--- src/main/java/com/teamtreehouse/web/controller/RecipeController.java
+++ src/main/java/com/teamtreehouse/web/controller/RecipeController.java
@@ -1,4 +1,29 @@
 package com.teamtreehouse.web.controller;
 
+import com.teamtreehouse.domain.Recipe;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class RecipeController {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    // Index of all recipes
+    @SuppressWarnings("unchecked")
+    @RequestMapping("/")
+    public String listRecipes(Model model) {
+        Session session = sessionFactory.openSession();
+        List<Recipe> recipes = session.createCriteria(Recipe.class).list();
+
+        model.addAttribute("recipes", recipes);
+        return "index";
+    }
 }
--- src/main/java/com/teamtreehouse/web/controller/UserController.java
+++ src/main/java/com/teamtreehouse/web/controller/UserController.java
@@ -1,4 +1,7 @@
 package com.teamtreehouse.web.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
 }
