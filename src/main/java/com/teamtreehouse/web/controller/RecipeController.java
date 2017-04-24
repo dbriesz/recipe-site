71bb217a9c9df03c7b54110c8a4003ca1daefc3c
David Riesz <dbriesz@users.noreply.github.com>
2017-04-24 16:34:30 -0700

    Add RecipeController methods (index, detail, add and edit) and tests

--- src/main/java/com/teamtreehouse/web/controller/RecipeController.java
+++ src/main/java/com/teamtreehouse/web/controller/RecipeController.java
@@ -5,6 +5,7 @@ import com.teamtreehouse.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class RecipeController {
     @Autowired
     private RecipeService recipeService;
 
-    // Index of all recipes
+    // Home page - index of all recipes
     @SuppressWarnings("unchecked")
     @RequestMapping("/")
     public String listRecipes(Model model) {
@@ -23,4 +24,41 @@ public class RecipeController {
         model.addAttribute("recipes", recipes);
         return "index";
     }
+
+    // Single recipe page
+    @RequestMapping("recipes/{recipeId}")
+    public String recipe(@PathVariable Long recipeId, Model model) {
+        // Get the recipe given by recipeId
+        Recipe recipe = recipeService.findById(recipeId);
+
+        model.addAttribute("recipe", recipe);
+
+        return "detail";
+    }
+
+    // Form for adding a new recipe
+    @RequestMapping("recipes/add")
+    public String formNewRecipe(Model model) {
+        // Add model attributes needed for new form
+        if (!model.containsAttribute("recipe")) {
+            model.addAttribute("recipe", new Recipe());
+        }
+        model.addAttribute("action", "/recipes/add");
+
+        return "/";
+    }
+
+    // Edit an existing recipe
+    @RequestMapping("recipes/{recipeId}/edit")
+    public String editRecipe(@PathVariable Long recipeId, Model model) {
+        // Add model attributes needed for edit form
+        Recipe recipe = recipeService.findById(recipeId);
+        if (!model.containsAttribute("recipe")) {
+            model.addAttribute("recipe", recipe);
+        }
+        model.addAttribute("action", String.format("/recipes/%s/edit", recipeId));
+
+        return "edit";
+    }
+
 }
---
+++ src/test/java/com/teamtreehouse/web/controller/RecipeControllerTest.java
@@ -0,0 +1,109 @@
+package com.teamtreehouse.web.controller;
+
+import com.teamtreehouse.domain.Category;
+import com.teamtreehouse.domain.Ingredient;
+import com.teamtreehouse.domain.Instruction;
+import com.teamtreehouse.domain.Recipe;
+import com.teamtreehouse.service.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class RecipeControllerTest {
+
+    final String BASE_URL = "http://localhost:8080";
+
+    @Mock
+    private MockHttpSession session;
+
+    @Mock
+    private RecipeService recipeService;
+
+    @InjectMocks
+    private RecipeController recipeController;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+    }
+
+    @Test
+    public void getIndex() throws Exception {
+        Recipe recipe = recipeBuilder();
+        Recipe recipe2 = recipeBuilder();
+
+        List<Recipe> recipes = new ArrayList<>();
+        recipes.add(recipe);
+        recipes.add(recipe2);
+
+        when(recipeService.findAll()).thenReturn(recipes);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void getRecipeDetail() throws Exception {
+        Recipe recipe = recipeBuilder();
+
+        when(recipeService.findById(1L)).thenReturn(recipe);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipes/1"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("detail"));
+    }
+
+    @Test
+    public void addNewRecipeTest() throws Exception {
+        Recipe recipe = recipeBuilder();
+
+        when(recipeService.findById(1L)).thenReturn(recipe);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/recipes/add"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("/"));
+    }
+
+
+
+    private Recipe recipeBuilder() {
+        Recipe recipe = new Recipe();
+        recipe.setName("TestName");
+        recipe.setDescription("TestDesc");
+        recipe.setCategory(new Category("TestCategory"));
+        recipe.setImageUrl("TestUrl");
+        recipe.addIngredient(new Ingredient());
+        recipe.addInstruction(new Instruction());
+        recipe.setPrepTime("10 minutes");
+        recipe.setCookTime("1 hour");
+        recipe.setFavorite(true);
+        return recipe;
+    }
+
+}
