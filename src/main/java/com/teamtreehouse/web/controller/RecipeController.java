e67c82d2f2c01a5c61b3ba42d1b4e64df3408d42
David Riesz <dbriesz@users.noreply.github.com>
2017-04-24 11:58:49 -0700

    Fix application failed to start error by removing Hibernate core dependency

--- src/main/java/com/teamtreehouse/web/controller/RecipeController.java
+++ src/main/java/com/teamtreehouse/web/controller/RecipeController.java
@@ -1,27 +1,24 @@
 package com.teamtreehouse.web.controller;
 
 import com.teamtreehouse.domain.Recipe;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import com.teamtreehouse.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class RecipeController {
     @Autowired
-    private SessionFactory sessionFactory;
+    private RecipeService recipeService;
 
     // Index of all recipes
     @SuppressWarnings("unchecked")
     @RequestMapping("/")
     public String listRecipes(Model model) {
-        Session session = sessionFactory.openSession();
-        List<Recipe> recipes = session.createCriteria(Recipe.class).list();
+        List<Recipe> recipes = recipeService.findAll();
 
         model.addAttribute("recipes", recipes);
         return "index";
